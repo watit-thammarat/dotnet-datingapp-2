e65b9e7551465eac40eb797cb46415de325bb9f2
Watit Thammarat <tongnakub@hotmail.com>
2021-10-31 00:40:28 +0700

    end of section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,52 @@
+using System;
+using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult GetSecret()
+        {
+            return Ok("secret text");
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if (thing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+            var thingToReturn = thing.ToString();
+
+            return Ok(thingToReturn);
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest("This was a bad request");
+        }
+    }
+}
